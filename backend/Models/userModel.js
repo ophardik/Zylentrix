1a0735ff15ff94dac43fa17b46a663f27a7f65d2
ophardik <116378316+ophardik@users.noreply.github.com>
2025-03-12 21:53:14 +0530

    Added .gitignore and removed node_modules from tracking

---
+++ backend/Models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose");
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true,
+    },
+    age:{
+        type:Number,
+        required:true,
+    },
+})
+const userModel=mongoose.model("User",userSchema);
+module.exports=userModel;
